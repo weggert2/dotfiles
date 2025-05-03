a8363757f228bc4b2d997c2c1c135e88febda1c5
Bill Eggert <bill.eggert@fireflyspace.com>
2025-05-02 22:05:21 -0500

    Add oh-my-zsh aliases file

---
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,25 @@
+alias upgrade="sudo aptitude update && sudo aptitude upgrade && sudo snap refresh"
+alias alpha_queue="sudo sysctl -w fs.mqueue.msg_max=256"
+alias fprime_mmap="sudo sysctl vm.mmap_rnd_bits=28"
+alias ac="cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && ln -sf build/compile_commands.json compile_commands.json"
+alias ab="ac && cmake --build build --parallel 12"
+alias at="ab && ctest --test-dir build"
+alias runtest='function _runtest() { make "$1" && (./"$1" || gdb ./"$1") ; }; _runtest'
+alias hash_alpha_bins="cmake -S . -B build -DCMAKE_BUILD_TYPE=Release && cmake --build build --parallel 10 && find build/bin -type f -name '*.bin' | xargs -I {} md5sum {}"
+alias etest="setarch `uname -m` -R bash -c \"fprime-util check -j16 --pass-through --rerun-failed --output-on-failure\""
+alias sourcez="exec zsh"
+alias vim="nvim"
+alias vi="nvim"
+alias v="nvim"
+alias v.="nvim ."
+
+teeclip() {
+  if [ "$XDG_SESSION_TYPE" = "wayland" ] && command -v wl-copy >/dev/null 2>&1; then
+    tee >(wl-copy)
+  elif [ "$DISPLAY" ] && command -v xclip >/dev/null 2>&1; then
+    tee >(xclip -selection clipboard)
+  else
+    echo "No clipboard tool found (needs wl-copy or xclip)" >&2
+    tee
+  fi
+}
