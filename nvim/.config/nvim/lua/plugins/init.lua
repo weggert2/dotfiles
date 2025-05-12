89c9477f8281b4ff8eee42fbb75b2922fc658327
Bill Eggert <beggert@protonmail.com>
2025-05-12 17:57:44 -0500

    Added lualine

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -448,4 +448,27 @@ return {
             })
         end,
     },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup({
+                options = {
+                    theme = 'auto',
+                    icons_enabled = false,
+                    section_separators = '',
+                    component_separators = '',
+                    globalstatus = true,
+                },
+                sections = {
+                    lualine_a = { 'mode' },
+                    lualine_b = {},
+                    lualine_c = { 'filename' },
+                    lualine_x = { 'filetype' },
+                    lualine_y = {},
+                    lualine_z = { 'location' },
+                },
+            })
+        end,
+    },
 }
